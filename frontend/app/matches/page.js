c508d24f4b9f90b76637414e2bdfbc59b618e1b1
LukeHecker <heckerle@miamioh.edu>
2026-06-01 22:10:19 -0500

    Cleanup: remove debug console.log

--- frontend/app/matches/page.js
+++ frontend/app/matches/page.js
@@ -27,7 +27,6 @@ export default function Matches() {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/jobs/matches`)
       .then(res => res.json())
       .then(data => {
-        console.log('matches data:', JSON.stringify(data[0]))
         cachedMatches = data
         setMatches(data)
         setLoading(false)
